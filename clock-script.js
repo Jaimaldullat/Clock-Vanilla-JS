c94d94ddd07525facbbd29c90374479cfea10449
Jaimal Dullat <jaimaldullat@gmail.com>
2019-02-25 14:57:57 -0500

    Added Tick Sound

--- clock-script.js
+++ clock-script.js
@@ -2,6 +2,7 @@
     const secondHand = document.querySelector('.second-hand');
     const minuteHand = document.querySelector('.minute-hand');
     const hourHand = document.querySelector('.hour-hand');
+    const tickSound = document.querySelector(".tick");
 
     function setDate() {
         const now = new Date();
@@ -16,6 +17,7 @@
         const hours = now.getHours();
         const hoursDegree = ((hours / 24) * 360) + 90;
         hourHand.style.transform = `rotate(${hoursDegree}deg)`;
+        tickSound.play();
     }
 
     setInterval(setDate, 1000);
