8703b57eee6fae9244308f64f21faf7574b30798
Jaimal Dullat <jaimaldullat@gmail.com>
2019-02-25 14:43:15 -0500

    Finished Clock using Simple Vanilla JS

---
+++ clock-script.js
@@ -0,0 +1,22 @@
+(function() {
+    const secondHand = document.querySelector('.second-hand');
+    const minuteHand = document.querySelector('.minute-hand');
+    const hourHand = document.querySelector('.hour-hand');
+
+    function setDate() {
+        const now = new Date();
+        const seconds = now.getSeconds();
+        const secondsDegree = ((seconds / 60) * 360) + 90;
+        secondHand.style.transform = `rotate(${secondsDegree}deg)`;
+
+        const minutes = now.getMinutes();
+        const minutesDegree = ((minutes / 60) * 360) + 90;
+        minuteHand.style.transform = `rotate(${minutesDegree}deg)`;
+
+        const hours = now.getHours();
+        const hoursDegree = ((hours / 24) * 360) + 90;
+        hourHand.style.transform = `rotate(${hoursDegree}deg)`;
+    }
+
+    setInterval(setDate, 1000);
+}());
